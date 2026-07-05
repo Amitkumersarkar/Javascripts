8e1e450ca994c3fa6a2aa43da9d7aba4e3fa8d6c
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-07-06 00:25:12 +0600

    basic array

---
+++ RecapBasicJs/basicArray.js
@@ -0,0 +1,8 @@
+const friends = ["amit", "niloy", "antik"];
+// friends.push("nevir"); //push method used to add new values
+// friends.pop(); //pop method used to remove something from last index
+// friends.unshift(); //unshift method used to remove something from first line of index
+// friends.shift(); //shift method used to add something on the first index
+// friends.slice(2,3); //The slice() method slices out a piece of an array into a new array
+console.log(friends[3]);
+console.log(friends.splice(2, 3));
--- RecapBasicJs/basicProbgram.js
+++ RecapBasicJs/basicProbgram.js
@@ -7,10 +7,3 @@ console.log(name);
 // const are fixed and we can't reassign it's value
 const country = "Bangladesh";
 console.log(country);
-// use of if else statement
-const num = 20;
-if (num > 20) {
-    console.log("Yes\n");
-} else {
-    console.log("No");
-}
