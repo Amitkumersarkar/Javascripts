b284f71260163da911955cf6eeca5b7ffb8659e8
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-07-06 00:09:21 +0600

    basic program

---
+++ RecapBasicJs/basicConditionalStatement.js
@@ -0,0 +1,7 @@
+// use of if else statement
+const num = 20;
+if (num > 20) {
+    console.log("Yes\n");
+} else {
+    console.log("No");
+}
---
+++ RecapBasicJs/basicProbgram.js
@@ -0,0 +1,16 @@
+// using let and const and var
+// let value are changeable
+let name = "Sarkar";
+name = "Amit Sarkar";
+console.log(name);
+
+// const are fixed and we can't reassign it's value
+const country = "Bangladesh";
+console.log(country);
+// use of if else statement
+const num = 20;
+if (num > 20) {
+    console.log("Yes\n");
+} else {
+    console.log("No");
+}
