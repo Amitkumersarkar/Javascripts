813586c96a7c18a3870fdc67f172cfcb0c9d3fb3
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-07-13 00:21:09 +0600

    method

--- RecapBasicJs/arraymethod.js
+++ RecapBasicJs/arraymethod.js
@@ -1,9 +1,12 @@
 const products = [
     { name: "xiaomi note 15 pro", brand: "Xiaomi", color: "blue", price: 500 },
-    { name: "Iphone 17 pro max", brand: "Xiaomi", color: "blue", price: 1500 },
-    { name: "vivo x300 pro", brand: "Xiaomi", color: "blue", price: 900 },
+    { name: "Iphone 17 pro max", brand: "apple", color: "blue", price: 1500 },
+    { name: "vivo x300 pro", brand: "vivo", color: "blue", price: 900 },
     { name: "samsung galaxy s26 ultra", brand: "samsung", color: "blue", price: 1200 }
 ]
 
-const items = products.map(prod => prod.price);
+// const items = products.map(prod => prod.price);
+// console.log(items);
+
+const items = products.filter(prod => prod.price >= 1000);
 console.log(items);
