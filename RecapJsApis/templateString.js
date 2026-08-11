f4608e6b19fa3e4e46aa4b862b1606cb91b8973a
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-08-11 22:43:28 +0600

    dynamic value

--- RecapJsApis/templateString.js
+++ RecapJsApis/templateString.js
@@ -1,7 +1,7 @@
 function sum(num1, num2) {
     const result = num1 + num2;
     // const output = 'sum of ' + num1 + ' and ' + num2 + ' is equal to ' + result;
-    const output = `Sum of ${num1} and ${num2}`
+    const output = `Sum of ${num1} and ${num2} is equal to ${result}`
     console.log(output);
 }
 sum(10, 20);
