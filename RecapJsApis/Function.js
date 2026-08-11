f34a8c708f33a7b4ef11579de02d015a6fae609a
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-08-11 23:05:38 +0600

    functions

---
+++ RecapJsApis/Function.js
@@ -0,0 +1,12 @@
+// function declaration
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+// function expression
+const addition = function (num1, num2) {
+    return num1 + num2;
+}
+
+const result = addition(17, 13);
+console.log(result);
