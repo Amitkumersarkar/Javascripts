2f73026add1e220e19fa876832ed3fc73ccd5243
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-08-13 18:20:25 +0600

    updated

--- RecapJsApis/arrowFun.js
+++ RecapJsApis/arrowFun.js
@@ -1,5 +1,10 @@
+// multi parameter
 const getTax = (amount, taxRate) => amount * taxRate / 100;
 const add = (p, q) => p + q;
-
+const firstElement = nums => nums[0];
+console.log(firstElement([29, 65, 47]))
 // single parameter
+const getSquare = (x) => x * x;
+console.log(getSquare(8));
 
+document.getElementById('btn-click').addEventListener('click', e = {})
