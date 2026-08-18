228ba49d312a26748c15c2e849e3fff55d27f804
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-08-18 23:47:42 +0600

    update

--- RecapJsApis/const.js
+++ RecapJsApis/const.js
@@ -2,6 +2,6 @@
 const name = 'Dhaka';
 const countryName = name + '-BD';
 console.log(countryName);
-// const student = { name: 'name', marks: 50 }
-// student.marks = 60;
-// console.log(marks);
+const student = { name: 'name', marks: 50 }
+student.marks = 60;
+console.log(marks);
